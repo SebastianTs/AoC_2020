caced3355b9d6b484758123e53c0694299f3bc91
sebastian <sebastian@phobos.speedport.ip>
2020-12-07 21:03:30 +0100

    solved day07 part 1

--- day07/main.go
+++ day07/main.go
@@ -19,24 +19,36 @@ type required struct {
 }
 
 func main() {
-	rules, err := readInput("./test-input")
+	rules, err := readInput("./input")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(findBag("shiny gold", rules))
-
 }
 
-func findBag(bag string, rules []rule) int {
-	results := []string{}
+func rulesToGraph(rules []rule) *Graph {
+	g := NewGraph()
 	for _, rule := range rules {
-		for _, allowed := range rule.reqs {
-			if bag == allowed.bag {
-				results = append(results, bag)
+		g.AddNode(rule.bag)
+	}
+	for _, rule := range rules {
+		for _, baginList := range rule.reqs {
+			err := g.AddEdge(baginList.bag, rule.bag, baginList.amount)
+			if err != nil {
+				panic(err)
 			}
 		}
 	}
-	return len(results)
+	return g
+}
+
+func findBag(bag string, rules []rule) (count int) {
+	g := rulesToGraph(rules)
+	result, err := g.TopSort(bag)
+	if err != nil {
+		panic(err)
+	}
+	return len(result) - 1
 }
 
 func readInput(filename string) (rules []rule, err error) {
@@ -66,4 +78,127 @@ func readInput(filename string) (rules []rule, err error) {
 		rules = append(rules, r)
 	}
 	return rules, nil
+}
+
+/* https://gostudent.github.io/Letsgo/Implementation-of-Topological-sort-in-GO */
+
+type Graph struct {
+	nodes map[string]node
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		nodes: make(map[string]node),
+	}
+}
+
+func (g *Graph) AddNode(name string) {
+	if !g.ContainsNode(name) {
+		g.nodes[name] = make(node)
+	}
+}
+
+func (g *Graph) AddEdge(from string, to string, weight int) error {
+	f, ok := g.nodes[from]
+	if !ok {
+		return fmt.Errorf("Node %q not found", from)
+	}
+	_, ok = g.nodes[to]
+	if !ok {
+		return fmt.Errorf("Node %q not found", to)
+	}
+
+	f.addEdge(to, weight)
+	return nil
+}
+
+func (g *Graph) ContainsNode(name string) bool {
+	_, ok := g.nodes[name]
+	return ok
+}
+
+func (g *Graph) TopSort(name string) ([]string, error) {
+	results := newOrderedSet()
+	err := g.visit(name, results, nil)
+	if err != nil {
+		return nil, err
+	}
+	return results.items, nil
+}
+
+func (g *Graph) visit(name string, results *orderedset, visited *orderedset) error {
+	if visited == nil {
+		visited = newOrderedSet()
+	}
+
+	added := visited.add(name)
+	if !added {
+		index := visited.index(name)
+		cycle := append(visited.items[index:], name)
+		return fmt.Errorf("Cycle error: %s", strings.Join(cycle, " -> "))
+	}
+
+	n := g.nodes[name]
+	for _, edge := range n.edges() {
+		err := g.visit(edge, results, visited.copy())
+		if err != nil {
+			return err
+		}
+	}
+
+	results.add(name)
+	return nil
+}
+
+type node map[string]int
+
+func (n node) addEdge(name string, weight int) {
+	n[name] = weight
+}
+
+func (n node) edges() []string {
+	var keys []string
+	for k := range n {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+type orderedset struct {
+	indexes map[string]int
+	items   []string
+	length  int
+}
+
+func newOrderedSet() *orderedset {
+	return &orderedset{
+		indexes: make(map[string]int),
+		length:  0,
+	}
+}
+
+func (s *orderedset) add(item string) bool {
+	_, ok := s.indexes[item]
+	if !ok {
+		s.indexes[item] = s.length
+		s.items = append(s.items, item)
+		s.length++
+	}
+	return !ok
+}
+
+func (s *orderedset) copy() *orderedset {
+	clone := newOrderedSet()
+	for _, item := range s.items {
+		clone.add(item)
+	}
+	return clone
+}
+
+func (s *orderedset) index(item string) int {
+	index, ok := s.indexes[item]
+	if ok {
+		return index
+	}
+	return -1
 }
