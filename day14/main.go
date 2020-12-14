86bc22e66667696d6be3d9f7563073e93b8735ee
sebastian tschiedel <s.tschiedel@gmail.com>
2020-12-14 18:09:18 +0100

    started part2

--- day14/main.go
+++ day14/main.go
@@ -9,11 +9,12 @@ import (
 )
 
 func main() {
-	ins, err := readInput("./input")
+	ins, err := readInput("./test-input")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(run(ins))
+	fmt.Println(runVersion2(ins))
 }
 
 func run(ins []instruction) (sum int) {
@@ -39,6 +40,42 @@ func run(ins []instruction) (sum int) {
 	}
 	for _, v := range memory {
 		sum += v
+	}
+	return
+}
+
+func runVersion2(ins []instruction) (sum int) {
+	memory := make(map[int]int)
+	for _, in := range ins {
+		val1 := 0
+		val2 := 0
+		val3 := 0
+		valx := 0
+		for i, s := range in.mask {
+			if s == '1' {
+				val1 += int(math.Pow(2, float64(len(in.mask)-i-1)))
+			} else if s == '0' {
+				val3 += int(math.Pow(2, float64(len(in.mask)-i-1)))
+			} else if s == 'X' {
+				valx += int(math.Pow(2, float64(len(in.mask)-i-1)))
+			}
+			val2 += int(math.Pow(2, float64(len(in.mask)-i-1)))
+		}
+		valmask := val2 ^ val3
+		for i := 0; i < 36; i++ {
+			cur := int(math.Pow(2, float64(i))) | valx
+			fmt.Printf("cur %b\n int %b\nvalx %b\n", cur, int(math.Pow(2, float64(i))), valx)
+			for _, m := range in.mem {
+				m[1] = m[1] & valmask
+				m[1] = m[1] | val1
+				m[0] = m[0] | cur
+				memory[m[0]] = m[1]
+			}
+		}
+	}
+	for k, v := range memory {
+		sum += v
+		fmt.Println(k, v)
 	}
 	return
 }
